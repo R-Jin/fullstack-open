76969fce94bac59534c9a1b45e4d55aae2bf6a2a
Ryan Jin <ryanjin007@gmail.com>
2024-05-28 23:33:59 +0200

    Concat new person to persons list before setting
    This change was made due to the api change. POST response does not
    return all persons. It now returns only the newly added person.

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,73 +1,96 @@
-import personService from '../services/persons'
+import personService from "../services/persons";
 
-const PersonForm = ({newName, setNewName, newNumber, setNewNumber, persons, setPersons, setStatusMessage, setErrorMessage}) => {
-
+const PersonForm = ({
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+  persons,
+  setPersons,
+  setStatusMessage,
+  setErrorMessage,
+}) => {
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // Check if person is in phone book
-    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase(),
+      )
+    ) {
       // Ask if we want to update the phone number
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        const duplicatePerson = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
-        const duplicateId = duplicatePerson.id
-        const updatedPerson = {name: duplicatePerson.name, number: newNumber}
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`,
+        )
+      ) {
+        const duplicatePerson = persons.find(
+          (person) => person.name.toLowerCase() === newName.toLowerCase(),
+        );
+        const duplicateId = duplicatePerson.id;
+        const updatedPerson = { name: duplicatePerson.name, number: newNumber };
         personService
           .updatePerson(duplicateId, updatedPerson)
-          .then(updatePerson => {
-            const updatedPersons = persons.map(person => person.id === duplicateId ? updatePerson : person)
-            setPersons(updatedPersons)
-            setStatusMessage(`Updated number of ${updatedPerson.name}`)
+          .then((updatePerson) => {
+            const updatedPersons = persons.map((person) =>
+              person.id === duplicateId ? updatePerson : person,
+            );
+            setPersons(updatedPersons);
+            setStatusMessage(`Updated number of ${updatedPerson.name}`);
             setTimeout(() => {
-              setStatusMessage(null)
-            }, 5000)
+              setStatusMessage(null);
+            }, 5000);
           })
-          .catch(error => {
-            setErrorMessage(`Information of ${updatedPerson.name} has already been removed from the server`)
+          .catch((error) => {
+            setErrorMessage(
+              `Information of ${updatedPerson.name} has already been removed from the server`,
+            );
             setTimeout(() => {
-              setErrorMessage(null)
-            }, 5000)
-          })
-        setNewName("")
-        setNewNumber("")
+              setErrorMessage(null);
+            }, 5000);
+          });
+        setNewName("");
+        setNewNumber("");
       }
     } else {
       // Create person
-      const newPerson = {name: newName, number: newNumber}
-      personService
-        .createPerson(newPerson)
-        .then(newPersons => {
-          setPersons(newPersons)
-          setStatusMessage(`Added ${newName}`)
-          setTimeout(() => {
-            setStatusMessage(null)
-          }, 5000)
-        })
-      setNewName("")
-      setNewNumber("")
+      const newPerson = { name: newName, number: newNumber };
+      personService.createPerson(newPerson).then((newPerson) => {
+        setPersons(persons.concat(newPerson));
+        setStatusMessage(`Added ${newName}`);
+        setTimeout(() => {
+          setStatusMessage(null);
+        }, 5000);
+      });
+      setNewName("");
+      setNewNumber("");
     }
-  }
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   return (
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addPerson}>add</button>
-        </div>
-      </form>
-  )
-}
+    <form>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit" onClick={addPerson}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default PersonForm
+export default PersonForm;
+
