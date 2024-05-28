99d72588b7d9cbac62788f7a14f2282eb04e9e64
Ryan Jin <ryanjin007@gmail.com>
2024-05-28 23:16:49 +0200

    Formatting

--- part2/phonebook/src/components/Person.jsx
+++ part2/phonebook/src/components/Person.jsx
@@ -1,9 +1,13 @@
-const Person = ({person, id, handleDelete}) => {
-    return (
-        <>
-            <div>{person.name} {person.number} <button onClick={() => handleDelete(id, person.name)}>delete</button></div>
-        </>
-    )
-}
+const Person = ({ person, id, handleDelete }) => {
+  return (
+    <>
+      <div>
+        {person.name} {person.number}{" "}
+        <button onClick={() => handleDelete(id, person.name)}>delete</button>
+      </div>
+    </>
+  );
+};
 
-export default Person
+export default Person;
+
